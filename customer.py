d02b7bdf8116cae1a5e218ee4153dce5cafc9f57
jun117 <kbokro@icloud.com>
2022-01-27 22:25:35 +0900

    C-8まで
    最後まできたが、オブジェクト指向理解できてません

--- customer.py
+++ customer.py
@@ -1,5 +1,5 @@
 class Customer:
-    def __init__(self, first_name, family_name,age):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
         self.age = age
@@ -8,36 +8,32 @@ class Customer:
         return (f"{self.first_name} {self.family_name}")
 
     def entry_fee(self):
-        if self.age <= 3 :
+        if self.age <= 3:
             return 0
-        elif 3 < self.age < 20 :
+        elif 3 < self.age < 20:
             return 1000
-        elif 20 <= self.age < 65 :
+        elif 20 <= self.age < 65:
             return 1500
-        elif 65<= self.age <75 :
+        elif 65 <= self.age < 75:
             return 1200
         else:
             return 500
 
     def info_csv(self):
-         return(f"{self.first_name} {self.family_name},{self.age},{self.entry_fee()}")
+        return (f"{self.first_name} {self.family_name},{self.age},{self.entry_fee()}")
 
     def tab(self):
-         return(f"{self.first_name} {self.family_name}\t{self.age}\t{self.entry_fee()}")
+        return (f"{self.first_name} {self.family_name}\t{self.age}\t{self.entry_fee()}")
 
     def pipe(self):
-         return(f"{self.first_name} {self.family_name}|{self.age}|{self.entry_fee()}")
-
-
-
-
+        return (f"{self.first_name} {self.family_name}|{self.age}|{self.entry_fee()}")
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-jun= Customer(first_name="Jun",family_name="Onuma", age=3)
-kin =Customer(first_name="Kin",family_name="Gin",age=100)
+jun = Customer(first_name="Jun", family_name="Onuma", age=3)
+kin = Customer(first_name="Kin", family_name="Gin", age=100)
 
 # C-1
 print(ken.full_name())  # "Ken Tanaka" という値を返す
@@ -73,5 +69,3 @@ print(ieyasu.tab())
 print(ken.pipe())
 print(tom.pipe())
 print(ieyasu.pipe())
-
-
