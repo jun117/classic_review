aff6043ae5b534eda577151592bc0d073c703ce3
jun117 <kbokro@icloud.com>
2022-01-27 22:19:24 +0900

    C-8まで
    最後まできたが、オブジェクト指向理解できてません

--- customer.py
+++ customer.py
@@ -8,14 +8,25 @@ class Customer:
         return (f"{self.first_name} {self.family_name}")
 
     def entry_fee(self):
-        if self.age < 20 :
+        if self.age <= 3 :
+            return 0
+        elif 3 < self.age < 20 :
             return 1000
-        elif self.age >= 65 :
+        elif 20 <= self.age < 65 :
+            return 1500
+        elif 65<= self.age <75 :
             return 1200
         else:
-            return 1500
+            return 500
+
     def info_csv(self):
          return(f"{self.first_name} {self.family_name},{self.age},{self.entry_fee()}")
+
+    def tab(self):
+         return(f"{self.first_name} {self.family_name}\t{self.age}\t{self.entry_fee()}")
+
+    def pipe(self):
+         return(f"{self.first_name} {self.family_name}|{self.age}|{self.entry_fee()}")
 
 
 
@@ -25,6 +36,8 @@ class Customer:
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+jun= Customer(first_name="Jun",family_name="Onuma", age=3)
+kin =Customer(first_name="Kin",family_name="Gin",age=100)
 
 # C-1
 print(ken.full_name())  # "Ken Tanaka" という値を返す
@@ -46,3 +59,19 @@ print(tom.info_csv())
 print(ieyasu.info_csv())
 
 # C-5
+print(jun.entry_fee())
+
+# C-6
+print(kin.entry_fee())
+
+# C-7
+print(ken.tab())
+print(tom.tab())
+print(ieyasu.tab())
+
+# C-8
+print(ken.pipe())
+print(tom.pipe())
+print(ieyasu.pipe())
+
+
